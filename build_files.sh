9a5870882ca84be753fb14ccd71b22a619efaeaa
lucachak <lucas.lucachak9@gmail.com>
2026-05-03 15:48:34 +0100

    done

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-echo "Building project..."
-python3.12 -m venv venv_build
-source venv_build/bin/activate
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-echo "Build complete."
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"
