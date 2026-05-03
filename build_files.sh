61c189c43be31dd2ccd9571b16b6278db267765a
lucachak <lucas.lucachak9@gmail.com>
2026-05-03 16:27:11 +0100

    fix: add break-system-packages to pip install

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "BUILD START"
 
-# Instala as dependências usando a versão correta do Python
-python3 -m pip install -r requirements.txt
+# Adicionamos a flag --break-system-packages para ignorar o bloqueio do Python 3.12
+python3 -m pip install -r requirements.txt --break-system-packages
 
 # Coleta os arquivos estáticos (CSS, JS, Imagens)
 python3 manage.py collectstatic --noinput --clear
