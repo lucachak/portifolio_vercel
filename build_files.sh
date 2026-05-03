52c911d88326ea2899edde7433fba9046db3ae0f
lucachak <lucas.lucachak9@gmail.com>
2026-05-03 15:46:14 +0100

    Fix Vercel build: use venv for collectstatic and align python version

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Building project..."
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
+python3.12 -m venv venv_build
+source venv_build/bin/activate
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
 echo "Build complete."
